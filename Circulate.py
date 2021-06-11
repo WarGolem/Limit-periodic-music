ea36dbb9ab2353bdc3d9eac31c7bed502a4b33bd
WarGolem <84435891+WarGolem@users.noreply.github.com>
2021-06-11 18:46:36 +0800

    Update Circulate.py

--- Circulate.py
+++ Circulate.py
@@ -1,15 +1,16 @@
 from pydub import AudioSegment
 
-input_music = AudioSegment.from_wav(f"D:\\FYP_Music\\BeatThee.0s.wav")  # Input original music file
-input_music_time = len(input_music)  # Save original music length as a parameter
+input_longest_music = AudioSegment.from_wav(f"D:\\FYP_Music\\BeatThee.6s.wav") # Input the longest music file
+music_period = len(input_longest_music)  # Save the longest music length as a parameter
 
 for i in range(7):
-    input_music = AudioSegment.from_wav(f"D:\\FYP_Music\\BeatThee.{i}s.wav")
-    output_music = input_music + input_music  # Combine two music segments into one
-    output_music.export(f"D:\\FYP_Music\\Circulated\\BeatThee.{i}c.wav", format="wav")
+    input_music = AudioSegment.from_wav(f"D:\\FYP_Music\\BeatThee.{i}s.wav") # Input each music file
+    output_music = input_music
     output_music_time = len(output_music)
 
-    while output_music_time < 2*input_music_time:   # To make each music segment have equal length
+    while output_music_time < music_period:   # To make each music segment have equal length
         output_music += input_music  # Add music segment to itself
         output_music.export(f"D:\\FYP_Music\\Circulated\\BeatThee.{i}c.wav", format="wav")  # Output final music file
         output_music_time = len(output_music)  # Compare the new music length with the assigned music length
+
+    output_music.export(f"D:\\FYP_Music\\Circulated\\BeatThee.{i}c.wav", format="wav")
